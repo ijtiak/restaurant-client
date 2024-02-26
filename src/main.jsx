ad8828baa578966a7be1d35bbaf60965331706d9
ijtiak <maad7445@gmail.com>
2024-02-26 19:30:52 +0600

    Home page done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Routes/Routes'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <div className='max-w-screen-lg mx-auto'>
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
   </React.StrictMode>,
 )
